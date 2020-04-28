83316f8123b0d71f169c3ac175576aadeda8b2a8
J.M. Wu <wumeibin1216@gmail.com>
2020-04-28 14:54:50 +0800

    [Init][1.0] ProjectInit

---
+++ Assets/Scripts/Sample.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using XLua;
+
+[Hotfix]
+public class Sample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
